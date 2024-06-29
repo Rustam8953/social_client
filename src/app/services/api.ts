d46d21f6902949b6c23f9e2846df0193b302d509
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-06-29 19:54:19 +0700

    endpoints project

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,16 +1,16 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../../const";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: `${BASE_URL}/api`,
-    prepareHeaders: (headers, {getState}) => {
+    /*prepareHeaders: (headers, {getState}) => {
         const token = (getState() as RootState).auth.token || localStorage.getItem("token")
         if(token) {
             headers.set('authorization', `Bearer ${token}`);
         }
         return headers;
-    }
+    }*/
 })
 const baseQueryWithRetry = retry(baseQuery, {maxRetries: 1})
 export const api = createApi({
