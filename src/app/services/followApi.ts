f6478b6a4e2f72b3fa588d60ec4614ab998a3a74
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-06-30 14:55:43 +0700

    follows/unfollow endpoints

---
+++ src/app/services/followApi.ts
@@ -0,0 +1,28 @@
+import { api } from "./api";
+
+export const followApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        follow: builder.mutation<void, {followingId: string}>({
+            query: (body) => ({
+                url: '/follow',
+                method: 'POST',
+                body
+            })
+        }),
+        unfollow: builder.mutation<void, string>({
+            query: (userId) => ({
+                url: `/unfollow/${userId}`,
+                method: 'DELETE'
+            })
+        })
+    })
+})
+
+export const {
+    useFollowMutation,
+    useUnfollowMutation
+} = followApi;
+
+export const {
+    endpoints: {follow, unfollow}
+} = followApi;
