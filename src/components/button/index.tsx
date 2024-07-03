73ba06283b1427b29933ce8ed0eea56bd44c376e
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-03 16:31:53 +0700

    structure button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import { Button as NextButton } from '@nextui-org/react';
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode;
+    icon: JSX.Element;
+    className?: string;
+    type?: 'button' | 'submit' | 'reset';
+    fullWidth?: boolean;
+    color?: "default" | "primary" | "secondary" | "success" | "warning" | "danger" | undefined;
+}
+
+export const Button = ({
+    children,
+    className,
+    icon,
+    color,
+    type,
+    fullWidth
+}: Props) => {
+    return (
+        <NextButton
+            startContent={icon}
+            size='lg'
+            color={color}
+            variant='light'
+            className={className}
+            type={type}
+            fullWidth={fullWidth}
+        >
+            {children}
+        </NextButton>
+    )
+}
