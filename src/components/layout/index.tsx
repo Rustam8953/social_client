b242754aa4db812cc17afc7aff68c58bf8b8c210
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-08 23:25:31 +0700

    redirect function unauth user

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from '../header';
 import { Container } from '../container';
 import { NavBar } from '../nav-bar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsAuthenticated, selectUser } from '../../features/user/user-slice';
 
 export const Layout = () => {
+  const isAuthenticate = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!isAuthenticate) {
+      navigate('/auth');
+    }
+  })
   return (
     <>
         <Header />
