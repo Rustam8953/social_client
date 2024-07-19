0ad50dc1f0b7c012e6d0a63a74e8dff79316766a
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-19 19:52:19 +0700

    create comment component add in page

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -3,6 +3,7 @@ import { useGetPostByIdQuery } from '../../app/services/postsApi';
 import { Card } from '../../components/card';
 import { Back } from '../../components/back';
 import { comment } from 'postcss';
+import { CreateComment } from '../../components/createComment';
 
 export const CurrentPost = () => {
   const params = useParams<{id: string}>();
@@ -36,21 +37,26 @@ export const CurrentPost = () => {
         likedByUser={likedByUser}
         createdAt={createdAt}
       />
-      {
-        data.comments ?
-          data.comments.map(i => (
-            <Card 
-              cardFor='comment'
-              key={i.id}
-              avatarUrl={i.user.avatarUrl ?? ''}
-              content={i.content}
-              name={i.user.name ?? ''}
-              authorId={i.userId}
-              commentId={i.id}
-              id={id}
-            />
-          )) : null
-      }
+      <div className="mt-10">
+        <CreateComment />
+      </div>
+      <div className="mt-10">
+        {
+          data.comments ?
+            data.comments.map(i => (
+              <Card 
+                cardFor='comment'
+                key={i.id}
+                avatarUrl={i.user.avatarUrl ?? ''}
+                content={i.content}
+                name={i.user.name ?? ''}
+                authorId={i.userId}
+                commentId={i.id}
+                id={id}
+              />
+            )) : null
+        }
+      </div>
     </>
   )
 }
