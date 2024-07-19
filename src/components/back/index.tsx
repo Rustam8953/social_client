2296538a7246bd414ac7d88673252dc3dc41254d
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-19 15:56:33 +0700

    back button

---
+++ src/components/back/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaRegArrowAltCircleLeft } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+export const Back = () => {
+    const navigate = useNavigate();
+    const handleBack = () => navigate(-1);
+    return (
+        <div 
+            onClick={handleBack}
+            className='text-default-500 flex items-center gap-2 mb-5 cursor-pointer'
+        >
+            <FaRegArrowAltCircleLeft />
+            Назад
+        </div>
+    )
+}
--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetPostByIdQuery } from '../../app/services/postsApi';
+import { Card } from '../../components/card';
+import { Back } from '../../components/back';
 
 export const CurrentPost = () => {
+  const params = useParams<{id: string}>();
+  const {data} = useGetPostByIdQuery(params?.id ?? '');
+
+  if(!data) return <h2>Поста не существует</h2>;
+
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likes,
+    author,
+    likedByUser,
+    createdAt
+  } = data;
+
   return (
-    <div>CurrentPost</div>
+    <>
+      <Back />
+      <Card 
+        cardFor='current-post'
+        avatarUrl={author.avatarUrl ?? ''}
+        content={content}
+        name={author.name ?? ''}
+        likesCount={likes.length}
+        commentsCount={comments.length}
+        authorId={authorId}
+        id={id}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+    </>
   )
 }
