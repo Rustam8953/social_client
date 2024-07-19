ed425000981fb6d11a206ad75ae32652b33acc62
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-19 19:51:17 +0700

    comment create component

---
+++ src/components/createComment/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useLazyGetPostByIdQuery } from '../../app/services/postsApi'
+import { Controller, useForm } from 'react-hook-form';
+import { Button, Textarea } from '@nextui-org/react';
+import { ErrorMessage } from '../errorMessge';
+import { IoMdCreate } from 'react-icons/io';
+import { useParams } from 'react-router-dom';
+import { useCreateCommentMutation } from '../../app/services/commentsApi';
+
+export const CreateComment = () => {
+    const {id} = useParams<{id: string}>();
+    const [createComment] = useCreateCommentMutation();
+    const [getPostById] = useLazyGetPostByIdQuery();
+
+    const {
+        handleSubmit,
+        control,
+        formState: {errors},
+        setValue
+    } = useForm();
+    const error = errors?.post?.message as string;
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            if(id) {
+                await createComment({content: data.comment, postId: id}).unwrap()
+                setValue('comment', '');
+                await getPostById(id).unwrap();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    return (
+        <form className='flex-grow' onSubmit={onSubmit}>
+            <Controller 
+                name='comment'
+                control={control}
+                defaultValue=''
+                rules={{
+                    required: "Обязательное поле!"
+                }}
+                render={({field}) => (
+                    <Textarea 
+                        {...field}
+                        labelPlacement='outside'
+                        placeholder='Ваш текст'
+                        className='mb-5'
+                    />
+                )}
+            />
+            {error && <ErrorMessage error={error} />}
+            <Button
+                color='primary'
+                className='flex-end'
+                endContent={<IoMdCreate />}
+                type='submit'
+            >
+                Добавить комментарий
+            </Button>
+        </form>
+    )
+}
