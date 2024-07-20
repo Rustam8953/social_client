7f9fb055cf831f632ae636df86e40347fc6abd2a
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-07-21 00:20:48 +0700

    button back component

--- src/components/back/index.tsx
+++ src/components/back/index.tsx
@@ -6,12 +6,11 @@ export const Back = () => {
     const navigate = useNavigate();
     const handleBack = () => navigate(-1);
     return (
-        <div 
-            onClick={handleBack}
-            className='text-default-500 flex items-center gap-2 mb-5 cursor-pointer'
-        >
-            <FaRegArrowAltCircleLeft />
-            Назад
+        <div onClick={handleBack} className='inline-block'>
+            <div className='text-default-500 flex items-center gap-2 mb-5 cursor-pointer'>
+                <FaRegArrowAltCircleLeft />
+                Назад
+            </div>
         </div>
     )
 }
